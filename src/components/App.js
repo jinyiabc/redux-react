dcb9be6e56509029eb18a7e05ef02ae4f82a5be0
Alex J.Y <13817282645@163.com>
2018-04-17 20:02:30 +0800

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+
+export default class App extends React.Component{
+    render(){
+        // build the users list
+        const users = [];
+        for(let i=1; i<10; i++){
+            users.push({
+                id:i,
+                username: 'John' +i,
+                job: 'Employee' +i
+            });
+        }
+
+        // render
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Username</th>
+                        <th>Job</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user,index) => {
+                        return (
+                            <tr key={user.id}>
+                                <td>#{user.id}</td>
+                                <td>{user.username}</td>
+                                <td>{user.job}</td>
+                                <td>
+                                    <a href={'/user-edit/' + user.id}>Eidt</a>
+                                </td>
+                                <td>
+                                    <button data-id={user.id}>Delete</button>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        );
+    }
+}
